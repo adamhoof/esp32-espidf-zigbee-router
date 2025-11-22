7f1e711533d1eb3c19dba33930e17ef21e4a784d
adamhoof <adam.hofman1@gmail.com>
2025-11-22 11:52:11 +0100

    formatting

--- main/main.c
+++ main/main.c
@@ -4,30 +4,9 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_zigbee_core.h"
+#include "zigbee_config.h"
 
-static const char *TAG = "ESP32_RANGE_EXTENDER";
-
-#define ESP_ZB_DEFAULT_RADIO_CONFIG()                                           \
-{                                                                               \
-.radio_mode = ZB_RADIO_MODE_NATIVE,                                             \
-}
-#define ESP_ZB_DEFAULT_HOST_CONFIG()                                            \
-{                                                                               \
-.host_connection_mode = ZB_HOST_CONNECTION_MODE_NONE,                           \
-}
-
-#define ESP_ZB_ZR_CONFIG()                                                      \
-    {                                                                           \
-      .esp_zb_role = ESP_ZB_DEVICE_TYPE_ROUTER,                                 \
-      .install_code_policy = false,                                             \
-      .nwk_cfg.zczr_cfg = {                                                     \
-          .max_children = 10,                                                   \
-        },                                                                      \
-    }
-
-
-static void esp_zb_task(void *pvParameters)
-{
+static void esp_zb_task(void *pvParameters) {
     esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZR_CONFIG();
 
     esp_zb_secur_network_min_join_lqi_set(0);
@@ -44,12 +23,7 @@ static void esp_zb_task(void *pvParameters)
 
     esp_zb_ep_list_t *ep_list = esp_zb_ep_list_create();
 
-    esp_zb_endpoint_config_t endpoint_config = {
-       .endpoint = 1,
-       .app_profile_id = ESP_ZB_AF_HA_PROFILE_ID,
-       .app_device_id = ESP_ZB_HA_RANGE_EXTENDER_DEVICE_ID,
-       .app_device_version = 0
-    };
+    esp_zb_endpoint_config_t endpoint_config = ESP_ZP_EP_CONFIG();
 
     esp_zb_ep_list_add_ep(ep_list, cluster_list, endpoint_config);
 
@@ -62,53 +36,53 @@ static void esp_zb_task(void *pvParameters)
     esp_zb_stack_main_loop();
 }
 
-void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct)
-{
+void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
     uint32_t *p_sg_p = signal_struct->p_app_signal;
     esp_err_t err_status = signal_struct->esp_err_status;
     esp_zb_app_signal_type_t sig_type = *p_sg_p;
 
     switch (sig_type) {
-    case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
-        ESP_LOGI(TAG, "Zigbee stack initialized");
-        esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
-        break;
+        case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
+            ESP_LOGI(TAG, "Zigbee stack initialized");
+            esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
+            break;
 
-    case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
-    case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
-        if (err_status == ESP_OK) {
-            ESP_LOGI(TAG, "Device started up in %s factory-reset mode", esp_zb_bdb_is_factory_new()? "" : "non");
-            if (esp_zb_bdb_is_factory_new()) {
-                ESP_LOGI(TAG, "Start Network Steering");
-                esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
+        case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
+        case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
+            if (err_status == ESP_OK) {
+                ESP_LOGI(TAG, "Device started up in %s factory-reset mode", esp_zb_bdb_is_factory_new()? "" : "non");
+                if (esp_zb_bdb_is_factory_new()) {
+                    ESP_LOGI(TAG, "Start Network Steering");
+                    esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
+                } else {
+                    ESP_LOGI(TAG, "Device restarted");
+                }
             } else {
-                ESP_LOGI(TAG, "Device restarted");
+                ESP_LOGW(TAG, "Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
             }
-        } else {
-            ESP_LOGW(TAG, "Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
-        }
-        break;
+            break;
 
-    case ESP_ZB_BDB_SIGNAL_STEERING:
-        if (err_status == ESP_OK) {
-            ESP_LOGI(TAG, "Joined network successfully");
-        } else {
-            ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
-            esp_zb_scheduler_alarm((esp_zb_callback_t)esp_zb_bdb_start_top_level_commissioning, ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
-        }
-        break;
+        case ESP_ZB_BDB_SIGNAL_STEERING:
+            if (err_status == ESP_OK) {
+                ESP_LOGI(TAG, "Joined network successfully");
+            } else {
+                ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
+                esp_zb_scheduler_alarm((esp_zb_callback_t) esp_zb_bdb_start_top_level_commissioning,
+                                       ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
+            }
+            break;
 
-    default:
-        ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type, esp_err_to_name(err_status));
-        break;
+        default:
+            ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type,
+                     esp_err_to_name(err_status));
+            break;
     }
 }
 
-void app_main(void)
-{
+void app_main(void) {
     esp_zb_platform_config_t config = {
-       .radio_config = ESP_ZB_DEFAULT_RADIO_CONFIG(),
-       .host_config = ESP_ZB_DEFAULT_HOST_CONFIG(),
+        .radio_config = ESP_ZB_DEFAULT_RADIO_CONFIG(),
+        .host_config = ESP_ZB_DEFAULT_HOST_CONFIG(),
     };
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_zb_platform_config(&config));
