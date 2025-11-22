4eea1b5657f79d8c9c6cebcb860a0819b52d8ff2
adamhoof <adam.hofman1@gmail.com>
2025-11-22 12:01:38 +0100

    file structure reorganization

--- main/main.c
+++ main/main.c
@@ -5,79 +5,9 @@
 #include "freertos/task.h"
 #include "esp_zigbee_core.h"
 #include "zigbee_config.h"
-
-static void esp_zb_task(void *pvParameters) {
-    esp_zb_cfg_t zb_nwk_cfg = ESP_ZB_ZR_CONFIG();
-
-    esp_zb_secur_network_min_join_lqi_set(0);
-
-    esp_zb_init(&zb_nwk_cfg);
-
-    esp_zb_cluster_list_t *cluster_list = esp_zb_zcl_cluster_list_create();
-
-    esp_zb_attribute_list_t *basic_cluster = esp_zb_basic_cluster_create(NULL);
-    esp_zb_cluster_list_add_basic_cluster(cluster_list, basic_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
-
-    esp_zb_attribute_list_t *identify_cluster = esp_zb_identify_cluster_create(NULL);
-    esp_zb_cluster_list_add_identify_cluster(cluster_list, identify_cluster, ESP_ZB_ZCL_CLUSTER_SERVER_ROLE);
-
-    esp_zb_ep_list_t *ep_list = esp_zb_ep_list_create();
-
-    esp_zb_endpoint_config_t endpoint_config = ESP_ZP_EP_CONFIG();
-
-    esp_zb_ep_list_add_ep(ep_list, cluster_list, endpoint_config);
-
-    esp_zb_device_register(ep_list);
-
-    esp_zb_set_primary_network_channel_set(ESP_ZB_TRANSCEIVER_ALL_CHANNELS_MASK);
-
-    ESP_ERROR_CHECK(esp_zb_start(false));
-
-    esp_zb_stack_main_loop();
-}
-
-void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
-    uint32_t *p_sg_p = signal_struct->p_app_signal;
-    esp_err_t err_status = signal_struct->esp_err_status;
-    esp_zb_app_signal_type_t sig_type = *p_sg_p;
-
-    switch (sig_type) {
-        case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
-            ESP_LOGI(TAG, "Zigbee stack initialized");
-            esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
-            break;
-
-        case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
-        case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
-            if (err_status == ESP_OK) {
-                ESP_LOGI(TAG, "Device started up in %s factory-reset mode", esp_zb_bdb_is_factory_new()? "" : "non");
-                if (esp_zb_bdb_is_factory_new()) {
-                    ESP_LOGI(TAG, "Start Network Steering");
-                    esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
-                } else {
-                    ESP_LOGI(TAG, "Device restarted");
-                }
-            } else {
-                ESP_LOGW(TAG, "Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
-            }
-            break;
-
-        case ESP_ZB_BDB_SIGNAL_STEERING:
-            if (err_status == ESP_OK) {
-                ESP_LOGI(TAG, "Joined network successfully");
-            } else {
-                ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
-                esp_zb_scheduler_alarm((esp_zb_callback_t) esp_zb_bdb_start_top_level_commissioning,
-                                       ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
-            }
-            break;
-
-        default:
-            ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type,
-                     esp_err_to_name(err_status));
-            break;
-    }
-}
+//this must be included even if it looks unused
+#include "signal_handler.h"
+#include "zigbee_task.h"
 
 void app_main(void) {
     esp_zb_platform_config_t config = {
