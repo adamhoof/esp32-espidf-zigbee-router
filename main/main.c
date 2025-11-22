1ff516069b3c9789387597f5226145851f7fc0b0
adamhoof <adam.hofman1@gmail.com>
2025-11-22 11:40:17 +0100

    formatting

--- main/main.c
+++ main/main.c
@@ -7,22 +7,22 @@
 
 static const char *TAG = "ESP32_RANGE_EXTENDER";
 
-#define ESP_ZB_DEFAULT_RADIO_CONFIG()                           \
-{                                                           \
-.radio_mode = ZB_RADIO_MODE_NATIVE,                     \
+#define ESP_ZB_DEFAULT_RADIO_CONFIG()                                           \
+{                                                                               \
+.radio_mode = ZB_RADIO_MODE_NATIVE,                                             \
 }
-#define ESP_ZB_DEFAULT_HOST_CONFIG()                            \
-{                                                           \
-.host_connection_mode = ZB_HOST_CONNECTION_MODE_NONE,   \
+#define ESP_ZB_DEFAULT_HOST_CONFIG()                                            \
+{                                                                               \
+.host_connection_mode = ZB_HOST_CONNECTION_MODE_NONE,                           \
 }
 
-#define ESP_ZB_ZR_CONFIG()                                          \
-    {                                                               \
-      .esp_zb_role = ESP_ZB_DEVICE_TYPE_ROUTER,                   \
-      .install_code_policy = false,           \
-      .nwk_cfg.zczr_cfg = {                                       \
-          .max_children = 10,                           \
-        },                                                          \
+#define ESP_ZB_ZR_CONFIG()                                                      \
+    {                                                                           \
+      .esp_zb_role = ESP_ZB_DEVICE_TYPE_ROUTER,                                 \
+      .install_code_policy = false,                                             \
+      .nwk_cfg.zczr_cfg = {                                                     \
+          .max_children = 10,                                                   \
+        },                                                                      \
     }
 
 
