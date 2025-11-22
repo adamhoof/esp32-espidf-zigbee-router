a5c9ebdaa21d23c9464aa7b226fd8df7b276d577
adamhoof <adam.hofman1@gmail.com>
2025-11-22 12:04:59 +0100

    bring back signal handler to main.c for less ambiguity

--- main/main.c
+++ main/main.c
@@ -5,9 +5,50 @@
 #include "freertos/task.h"
 #include "esp_zigbee_core.h"
 #include "zigbee_config.h"
-//this must be included even if it looks unused
-#include "signal_handler.h"
 #include "zigbee_task.h"
+
+void esp_zb_app_signal_handler(esp_zb_app_signal_t *signal_struct) {
+    uint32_t *p_sg_p = signal_struct->p_app_signal;
+    esp_err_t err_status = signal_struct->esp_err_status;
+    esp_zb_app_signal_type_t sig_type = *p_sg_p;
+
+    switch (sig_type) {
+        case ESP_ZB_ZDO_SIGNAL_SKIP_STARTUP:
+            ESP_LOGI(TAG, "Zigbee stack initialized");
+            esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_INITIALIZATION);
+            break;
+
+        case ESP_ZB_BDB_SIGNAL_DEVICE_FIRST_START:
+        case ESP_ZB_BDB_SIGNAL_DEVICE_REBOOT:
+            if (err_status == ESP_OK) {
+                ESP_LOGI(TAG, "Device started up in %s factory-reset mode", esp_zb_bdb_is_factory_new()? "" : "non");
+                if (esp_zb_bdb_is_factory_new()) {
+                    ESP_LOGI(TAG, "Start Network Steering");
+                    esp_zb_bdb_start_top_level_commissioning(ESP_ZB_BDB_MODE_NETWORK_STEERING);
+                } else {
+                    ESP_LOGI(TAG, "Device restarted");
+                }
+            } else {
+                ESP_LOGW(TAG, "Failed to initialize Zigbee stack (status: %s)", esp_err_to_name(err_status));
+            }
+            break;
+
+        case ESP_ZB_BDB_SIGNAL_STEERING:
+            if (err_status == ESP_OK) {
+                ESP_LOGI(TAG, "Joined network successfully");
+            } else {
+                ESP_LOGI(TAG, "Network steering was not successful (status: %s)", esp_err_to_name(err_status));
+                esp_zb_scheduler_alarm((esp_zb_callback_t) esp_zb_bdb_start_top_level_commissioning,
+                                       ESP_ZB_BDB_MODE_NETWORK_STEERING, 1000);
+            }
+            break;
+
+        default:
+            ESP_LOGI(TAG, "ZDO signal: %s (0x%x), status: %s", esp_zb_zdo_signal_to_string(sig_type), sig_type,
+                     esp_err_to_name(err_status));
+            break;
+    }
+}
 
 void app_main(void) {
     esp_zb_platform_config_t config = {
